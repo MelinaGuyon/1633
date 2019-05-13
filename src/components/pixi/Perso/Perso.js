08a8f6a30e242a404f94466d3351c245671eac25
Ptikiki <alicia.baudry.31@gmail.com>
2019-05-13 14:28:50 +0200

    add assets + perso animations

--- src/components/pixi/Perso/Perso.js
+++ src/components/pixi/Perso/Perso.js
@@ -3,16 +3,18 @@ import PixiComponent from 'abstractions/PixiComponent'
 import Animator from 'controllers/animator'
 import camera from 'controllers/camera'
 import physics from 'controllers/physics'
+import signals from 'state/signals'
 import Body from 'abstractions/Body'
 
 export default class Perso extends PixiComponent {
   setup () {
     this.base = new Container()
     this.refs = {}
+    this.oldDirection = null
 
-    this.refs.perso = this.addChild('elle-0/land')
+    this.refs.perso = this.addChild('animation-static/animation')
     this.anim = new Animator(this.refs.perso)
-    this.anim.play('elle-0/land', { loop: true, frameDuration: 80 })
+    this.anim.play('animation-static/animation', { loop: true, frameDuration: 600 })
     console.log(this.base.width, 'perso width')
     this.base.fakeX = 0
     this.base.fakeY = 0
@@ -29,6 +31,27 @@ export default class Perso extends PixiComponent {
     this.body.collideWith('obstacles', (c) => {
       // console.log('COLLIDE')
     })
+
+    this.bind()
+  }
+
+  bind () {
+    signals.goLeft.listen(this.updateAnimation, this) // 0
+    signals.goRight.listen(this.updateAnimation, this) // 1
+    signals.stop.listen(this.updateAnimation, this)
+  }
+
+  updateAnimation (direction) {
+    if (this.oldDirection !== direction) {
+      if (direction === 0) {
+        this.anim.play('animation-left/animation', { loop: true, frameDuration: 600 })
+      } else if (direction === 1) {
+        this.anim.play('animation-right/animation', { loop: true, frameDuration: 600 })
+      } else {
+        this.anim.play('animation-static/animation', { loop: true, frameDuration: 600 })
+      }
+      this.oldDirection = direction
+    }
   }
 
   update (dt, time) {
