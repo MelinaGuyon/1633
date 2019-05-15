7b78048da88e5144e9794af8882d667ee72b30cf
Jacques tatiana <tatout08090@gmail.com>
2019-05-15 13:11:18 +0200

    feat(intro) add base

--- src/components/dom/GameInterface/GameInterface.js
+++ src/components/dom/GameInterface/GameInterface.js
@@ -5,6 +5,7 @@ import Carrousel from 'components/dom/Carrousel/Carrousel'
 import Chronologie from 'components/dom/Chronologie/Chronologie'
 import Subtitles from 'components/dom/Subtitles/Subtitles'
 import MouseMv from 'components/dom/MouseMv/MouseMv'
+import Intro from 'components/dom/Intro/Intro'
 
 import './GameInterface.styl'
 
@@ -12,6 +13,7 @@ export default class GameInterface extends DomComponent {
   template ({ base }) {
     return (
       <section class='game'>
+        <Intro />
         <Menu />
         <Carrousel />
         <Chronologie />
