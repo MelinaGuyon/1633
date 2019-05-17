462f6bfcca29e4eab4b21a8a01018771fa53672f
Ptikiki <alicia.baudry.31@gmail.com>
2019-05-17 17:08:49 +0200

    nfdjfn

--- src/controllers/camera.js
+++ src/controllers/camera.js
@@ -32,7 +32,7 @@ class Camera {
     // C'est ici qu'est le reel X car il se fait animer en smooth
     if (this.target && this.target.hasMoved) {
       signals.moving.dispatch(this.x * -1)
-      console.log(this.x * -1)
+      // console.log(this.x * -1)
     }
   }
 
