68a18284584fbd9e0532d6a296b844a6f6221fc5
Melina Guyon <melina.guyon@stinkstudios.com>
2019-05-24 16:15:02 +0200

    Scene

--- src/components/pixi/LevelPharmacist/CityBuildings.js
+++ src/components/pixi/LevelPharmacist/CityBuildings.js
@@ -4,22 +4,12 @@ import Building from './CityBuilding'
 export default class CityBuildings extends PixiComponent {
   setup () {
     this.mains = []
-    // this.mains.push(this.addComponent(Building, { layer: '5bg100', x: 730, type: 'a', scale: 0.9, y: -50 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 800, type: 'b', y: -50, scale: 0.9 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg300', x: 800, type: 'c', scale: 0.9, y: -50 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 300, type: 'd', scale: 0.9, y: -100 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg300', x: 100, type: 'e', scale: 0.9, y: -150 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg400', x: 600, type: 'f', scale: 0.9, y: -50 }))
-
-
-    this.mains.push(this.addComponent(Building, { layer: '5bg100', x: 0, type: 'a', scale: 0.9, y: -50 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 0, type: 'b', y: -50, scale: 0.9 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg300', x: 0, type: 'c', scale: 0.9, y: -50 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 0, type: 'd', scale: 0.9, y: -100 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg300', x: 0, type: 'e', scale: 0.9, y: -150 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg400', x: 0, type: 'f', scale: 0.9, y: -50 }))
-    // this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 0, type: 'a', y: -150 }))
-    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 0, y: -162, tint: 0x77ddff }))
+    this.mains.push(this.addComponent(Building, { layer: '5bg400', x: 0, type: 'f', scale: 0.9, y: -50 })) // big
+    this.mains.push(this.addComponent(Building, { layer: '5bg100', x: 300, type: 'a', scale: 0.9, y: -50 }))
+    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 300, type: 'b', y: -50, scale: 0.9 }))
+    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: 300, type: 'c', scale: 0.9, y: -50 }))
+    this.mains.push(this.addComponent(Building, { layer: '5bg300', x: -120, type: 'd', scale: 0.9, y: -100 }))
+    this.mains.push(this.addComponent(Building, { layer: '5bg200', x: -400, type: 'e', scale: 0.9, y: -150 }))
   }
 
   componentWillUnmount () {
