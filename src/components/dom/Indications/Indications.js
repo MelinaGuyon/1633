c547c94e1b51ad78c51417370be9b8c1e2d25bb7
Melina Guyon <melina.guyon@stinkstudios.com>
2019-05-28 10:46:08 +0200

    Indications with locs

--- src/components/dom/Indications/Indications.js
+++ src/components/dom/Indications/Indications.js
@@ -1,12 +1,13 @@
 import { DomComponent } from 'abstractions/DomComponent'
 import { h, addRef } from '@internet/dom'
 import signals from 'state/signals'
+import store from 'state/store'
 
 import './Indications.styl'
 
 const locs = [
-  'Click and hold',
-  'Scroll'
+  'cta.hold',
+  'cta.scroll'
 ]
 
 export default class Indications extends DomComponent {
@@ -27,7 +28,8 @@ export default class Indications extends DomComponent {
   setIndication (indication) {
     if (indication === 0) this.indications.classList.remove('is-visible')
     else {
-      this.indications.innerText = locs[indication - 1]
+      const loc = store.loc.get()
+      this.indications.innerText = loc[locs[indication - 1]]
       this.indications.classList.add('is-visible')
     }
   }
