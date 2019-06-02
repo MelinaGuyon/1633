ff777d8671d89307376ad4827db0ec6623482447
Jacques tatiana <tatout08090@gmail.com>
2019-06-02 15:06:03 +0200

    WIP(voixoff) add intro

--- src/components/pixi/LevelUniversity/SoundChecks.js
+++ src/components/pixi/LevelUniversity/SoundChecks.js
@@ -11,10 +11,10 @@ export default class SoundChecks extends PixiComponent {
     // ic dans cb :
     // 0 = soundId car on veut lancer le son 0
     // 0 = collidersId car c'est le colliders 0 de cette classe
-    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -1300, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro', 0, 0), name: 'voixoff/chap_1' }))
-    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -1000, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro_bis', 0, 0), name: 'voixoff/chap_1' }))
-    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -800, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro_ter', 0, 0), name: 'voixoff/chap_1' }))
-    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -500, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/chap_1', 0, 0), name: 'voixoff/chap_1' }))
+    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -1300, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro', 1, 0), name: 'voixoff/chap_1' }))
+    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -1000, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro_bis', 2, 0), name: 'voixoff/chap_1' }))
+    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -800, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/intro_ter', 3, 0), name: 'voixoff/chap_1' }))
+    this.mains.push(this.addComponent(Colliders, { layer: '1bg100', x: -500, y: -100, group: 'sound', collide: true, tint: 0x000000, cb: this.cb.bind(this, 'voixoff/chap_1', 4, 0), name: 'voixoff/chap_1' }))
   }
 
   cb (path, soundId, collidersId, state) {
