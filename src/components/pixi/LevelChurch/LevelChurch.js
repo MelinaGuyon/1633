9ffa237e13c1ae4b8c68c80368051c0a36e44338
Melina Guyon <melina.guyon@stinkstudios.com>
2019-06-03 14:57:46 +0200

    Skip tuto

--- src/components/pixi/LevelChurch/LevelChurch.js
+++ src/components/pixi/LevelChurch/LevelChurch.js
@@ -6,7 +6,8 @@ import Buildings from './CityBuildings'
 import TutoCheck from './TutoChecks'
 import SoundCheck from './SoundChecks'
 import cookie from 'controllers/cookie'
-import LevelLight from "./LevelLight";
+import LevelLight from './LevelLight'
+import store from 'state/store';
 
 export default class LevelChurch extends Level {
   setup () {
@@ -23,7 +24,7 @@ export default class LevelChurch extends Level {
     this.addComponent(Interests)
 	  let isAlreadyShow
 	  isAlreadyShow = cookie.readCookie('tuto')
-	  if (!isAlreadyShow) {
+	  if (!isAlreadyShow && !store.skipTuto.get()) {
       this.addComponent(TutoCheck)
 	  }
 	  this.addComponent(SoundCheck)
--- src/state/store.js
+++ src/state/store.js
@@ -71,6 +71,7 @@ export default createStore({
   displayStats: isDev ? 1 : 0, // use stats (fps, drawcalls, ...)
   skipCarousel: isDev ? 1 : 1,
   skipLoading: isDev ? 1 : 0,
+  skipTuto: isDev ? 1 : 1,
 
   // ---- game ----
   loaded: false, // Is app loaded
