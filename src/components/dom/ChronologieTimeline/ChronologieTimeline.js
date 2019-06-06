fee996f9ef5ba93e3b019ed2b6691c0f61ecd30f
Melina Guyon <melina.guyon@stinkstudios.com>
2019-06-06 17:59:03 +0200

    Timeline mousehover

--- src/components/dom/ChronologieTimeline/ChronologieTimeline.js
+++ src/components/dom/ChronologieTimeline/ChronologieTimeline.js
@@ -7,10 +7,13 @@ import './ChronologieTimeline.styl'
 class Stick extends DomComponent {
   template (props) {
     return (
-      <div class='stick-container' ref={addRef(this, 'stick-container')}>
-        <div class='stick' ref={addRef(this, 'stick')} />
+      <div class='stick-container' data-id={props.id} ref={addRef(this, 'stickContainer')}>
+        <div class='stick ' ref={addRef(this, 'stick')} />
       </div>
     )
+  }
+
+  componentDidMount () {
   }
 }
 
@@ -37,7 +40,7 @@ export default class ChronologieTimeline extends DomComponent {
     return (
       <div class='chrono-timeline' ref={addRef(this, 'chronoTimeline')}>
         <div class='container'>
-          <span ref={addRef(this, 'span')}>1253</span>
+          <span ref={addRef(this, 'span')} />
           <div class='stick-global-container'>
             {sticks}
           </div>
@@ -56,13 +59,31 @@ export default class ChronologieTimeline extends DomComponent {
     this.listenStore('chronologieTimelineVisible', this.updateTimeline)
   }
 
+  internalBind () {
+    this.chronoTimeline.addEventListener('mouseleave', this.fastbind('reset', 1))
+    this.sticks.forEach((el) => {
+      el.base.addEventListener('mouseover', this.fastbind('stickMousehover', 1))
+    })
+  }
+
+  internalUnbind () {
+    this.chronoTimeline.removeEventListener('mouseleave', this.reset)
+    this.sticks.forEach((el) => {
+      el.base.removeEventListener('mouseover', this.stickMousehover)
+    })
+  }
+
   updateTimeline (visible) {
-    if (visible) this.chronoTimeline.classList.add('visible')
-    else this.chronoTimeline.classList.remove('visible')
+    if (visible) {
+      this.chronoTimeline.classList.add('visible')
+      this.internalBind()
+    } else {
+      this.chronoTimeline.classList.remove('visible')
+      this.internalUnbind()
+    }
   }
 
   updateCurrent (infos) {
-
     if (this.currentIndex !== infos.index) {
       let dateCompteur = this.startDate
       let date
@@ -80,20 +101,49 @@ export default class ChronologieTimeline extends DomComponent {
     }
   }
 
-  updateStick (text, opacity) {
+  updateStick (text, hovering) {
     this.sticks.forEach((el) => {
-      el.stick.style.transform = 'scaleX(0.22)'
+      if (!hovering)el.stick.style.transform = 'scaleX(0.22)'
     })
 
     if (this.sticks[this.currentStick - 2]) this.sticks[this.currentStick - 2].stick.style.transform = 'scaleX(0.5)'
     if (this.sticks[this.currentStick - 1]) this.sticks[this.currentStick - 1].stick.style.transform = 'scaleX(0.7)'
 
     this.sticks[this.currentStick].stick.style.transform = 'scaleX(1)'
-    this.span.style.transform = `translateY(calc(-51% + ${this.sticks[this.currentStick].base.offsetTop}px))`
-    this.span.innerText = text
-    // this.span.style.opacity = opacity
+    if (!hovering) {
+      this.span.style.transform = `translateY(calc(-51% + ${this.sticks[this.currentStick].base.offsetTop}px))`
+      this.span.innerText = text
+      this.text = text
+    }
 
     if (this.sticks[this.currentStick + 1]) this.sticks[this.currentStick + 1].stick.style.transform = 'scaleX(0.7)'
     if (this.sticks[this.currentStick + 2]) this.sticks[this.currentStick + 2].stick.style.transform = 'scaleX(0.5)'
+  }
+
+  stickMousehover (e) {
+    let id = Number(e.target.closest('.stick-container').getAttribute('data-id'))
+    this.updateStickHover(id)
+  }
+
+  updateStickHover (id) {
+    this.sticks.forEach((el, index) => {
+      if (index <= this.currentStick + 2 && index >= this.currentStick - 2) return
+      el.stick.style.transform = 'scaleX(0.22)'
+    })
+    this.updateStick('', true)
+
+    if (this.sticks[id - 2]) this.sticks[id - 2].stick.style.transform = 'scaleX(0.5)'
+    if (this.sticks[id - 1]) this.sticks[id - 1].stick.style.transform = 'scaleX(0.7)'
+
+    this.sticks[id].stick.style.transform = 'scaleX(1)'
+    this.span.style.transform = `translateY(calc(-51% + ${this.sticks[id].base.offsetTop}px))`
+    this.span.innerText = this.startDate + this.yearsPlage * id
+
+    if (this.sticks[id + 1]) this.sticks[id + 1].stick.style.transform = 'scaleX(0.7)'
+    if (this.sticks[id + 2]) this.sticks[id + 2].stick.style.transform = 'scaleX(0.5)'
+  }
+
+  reset () {
+    this.updateStick(this.text)
   }
 }
