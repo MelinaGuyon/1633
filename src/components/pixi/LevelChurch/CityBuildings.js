e15c93d01f980c05f495122168a478b61bb89a15
Melina Guyon <melina.guyon@stinkstudios.com>
2019-06-11 20:03:56 +0200

    scene foot

--- src/components/pixi/LevelChurch/CityBuildings.js
+++ src/components/pixi/LevelChurch/CityBuildings.js
@@ -7,15 +7,20 @@ export default class CityBuildings extends PixiComponent {
     this.mains.push(this.addComponent(Building, { layer: '2bg700', type: 'c', x: 0, y: -50, scale: 0.66 })) // fond incrusté
     this.mains.push(this.addComponent(Building, { layer: '2bg700', type: 'b', x: -15, y: -70, scale: 0.66 })) // fond
     this.mains.push(this.addComponent(Building, { layer: '2bg700', type: 'a', x: 0, y: -50, scale: 0.66 })) // big
+
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'd', x: 0, y: -20, scale: 0.66 })) // immeuble fond
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'e', x: 20, y: -50, scale: 0.66 })) // immeuble droite
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'f', x: -20, y: -50, scale: 0.66 })) // immeuble gauche
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'g', x: -180, y: 0, scale: 0.66 })) // puit
-    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'h', x: 65, y: -50, scale: 0.66 })) // bacshamp
-    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'i', x: -100, y: 150, scale: 0.66 })) // tete
-    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'j', x: -300, y: 90, scale: 0.66 })) // chien
+    this.mains.push(this.addComponent(Building, { layer: '2bg500', type: 'i', x: 35, y: 190, scale: 0.64 })) // tete
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'k', x: -20, y: -90, scale: 0.66 })) // lampe
     this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'l', x: -350, y: -220, scale: 0.66 })) // vetement
+
+    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'z', x: -168, y: -360, scale: 0.66 })) // baschamp
+    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'y', x: -700, y: -150, scale: 0.66 })) // joueur centre
+    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'x', x: -230, y: -50, scale: 0.66 })) // joueur left
+    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'w', x: -60, y: -250, scale: 0.66 })) // joueur right
+    this.mains.push(this.addComponent(Building, { layer: '2bg600', type: 'v', x: -434, y: -200, scale: 0.66 })) // femme gauche
   }
 
   componentWillUnmount () {
--- src/components/pixi/LevelUniversity/CityBuildings.js
+++ src/components/pixi/LevelUniversity/CityBuildings.js
@@ -16,19 +16,7 @@ export default class CityBuildings extends PixiComponent {
     this.mains.push(this.addComponent(Building, { layer: '1bg100', type: 'g', x: -450, y: 0 }))
     // this.mains.push(this.addComponent(Light, { layer: '1bg200', form: 'light/main', target: this.base, x: 0, y: -63, tint: 0x886600, alpha: 1, scale: [0.3, 0.27] }))
 
-    // armez  normalement 6bg..
-    // this.mains.push(this.addComponent(Building, { layer: '0bg700', type: 'a', x: -620, y: -325, scale: 0.66 })) // fond
-    // this.mains.push(this.addComponent(Building, { layer: '0bg700', type: 'c', x: -810, y: -450, scale: 0.78 })) // fond incrusté
-    // this.mains.push(this.addComponent(Building, { layer: '0bg700', type: 'b', x: -620, y: -330, scale: 0.66 })) // fond
-
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'd', x: -540, y: -200, scale: 0.66 })) // fenetre gauche
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'e', x: 245, y: -230, scale: 0.66 })) // fenetre droite
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'g', x: -880, y: -360, scale: 0.64 })) // bibliothèque
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'f', x: -700, y: -390, scale: 0.64 })) // bureau
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'h', x: 200, y: -225, scale: 0.64 })) // petit fils
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'i', x: -150, y: -310, scale: 0.58 })) // lampe
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'j', x: -350, y: -260, scale: 0.66 })) // chat
-    // this.mains.push(this.addComponent(Building, { layer: '0bg600', type: 'k', x: -196, y: -116, scale: 0.64 })) // armez
+    // armez 6bg..
   }
 
   componentWillUnmount () {
