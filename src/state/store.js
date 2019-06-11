953fd2c7c2ad81d4916ab91a977f4603d48c0024
Melina Guyon <melina.guyon@stinkstudios.com>
2019-06-11 23:05:39 +0200

    quick fix

--- src/state/store.js
+++ src/state/store.js
@@ -61,7 +61,7 @@ export default createStore({
   mute: isDev ? 0 : 0,
   useGui: isDev ? 1 : 0, // use datGui
   displayStats: isDev ? 1 : 0, // use stats (fps, drawcalls, ...)
-  skipCarousel: isDev ? 1 : 0,
+  skipCarousel: isDev ? 0 : 0,
   skipLoading: isDev ? 1 : 0,
   skipTuto: isDev ? 1 : 0,
 
