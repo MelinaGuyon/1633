0c00b90dda3ade97ded5ed419e9d16c73a7b289a
Ptikiki <alicia.baudry.31@gmail.com>
2019-06-13 16:56:27 +0200

    make Richelieu walking between plans

--- src/components/pixi/LevelUniversity/CityBuildings.js
+++ src/components/pixi/LevelUniversity/CityBuildings.js
@@ -14,9 +14,9 @@ export default class CityBuildings extends PixiComponent {
     this.mains.push(this.addComponent(Building, { layer: '1bg600', type: 'd', x: 0, y: -50, scale: 0.66 })) // fenetre
     this.mains.push(this.addComponent(Building, { layer: '1bg500', type: 'e', x: -20, y: -60, scale: 0.66 })) // meuf millieu droite
     this.mains.push(this.addComponent(Building, { layer: '1bg500', type: 'f', x: -170, y: -20, scale: 0.66 })) //  meuf gauche gauche
-    this.mains.push(this.addComponent(Building, { layer: '1bg500', type: 'h', x: -460, y: -360, scale: 0.62 })) //  mausolé
-    this.mains.push(this.addComponent(Building, { layer: '1f100', type: 'g', x: 440, y: -10, scale: 0.66 })) //  mec droite
-    this.mains.push(this.addComponent(Building, { layer: '1f200', type: 'i', x: -570, y: -280, scale: 0.66 })) //  mec gauche
+    this.mains.push(this.addComponent(Building, { layer: '1bg400', type: 'h', x: -460, y: -320, scale: 0.62 })) //  mausolé
+    this.mains.push(this.addComponent(Building, { layer: '1f100', type: 'g', x: 440, y: 20, scale: 0.66 })) //  mec droite
+    this.mains.push(this.addComponent(Building, { layer: '1f200', type: 'i', x: -570, y: -230, scale: 0.66 })) //  mec gauche
 
     // this.mains.push(this.addComponent(Light, { layer: '1bg400', form: 'light/main', target: this.base, x: 0, y: -63, tint: 0x886600, alpha: 1, scale: [0.3, 0.27] }))
   }
