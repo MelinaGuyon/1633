aa9bdeca4cb93c1295a4595804cfc220755f32e4
Melina Guyon <melina.guyon@stinkstudios.com>
2019-06-13 15:54:41 +0200

    Carousel animation rebuild at resize

--- src/components/dom/Carrousel/TextScrolling.js
+++ src/components/dom/Carrousel/TextScrolling.js
@@ -22,11 +22,34 @@ export default class TextScrolling extends DomComponent {
   }
 
   componentDidMount () {
+    this.create()
+    this.bind()
+  }
+
+  bind () {
+    this.listenStore('size', this.rebuild)
+    this.listenStore('launched', this.isLaunched)
+  }
+
+  unbind () {
+    this.unlistenStore('size', this.rebuild)
+  }
+
+  isLaunched (launched) {
+    if (launched) this.unbind()
+  }
+
+  create () {
     while (this.wrapper.offsetWidth < window.innerWidth) this.addSpan()
     this.wrapper.innerHTML += this.wrapper.innerHTML
   }
 
   addSpan () {
     this.wrapper.appendChild(this.span.cloneNode(true))
+  }
+
+  rebuild () {
+    while (this.wrapper.firstChild) this.wrapper.removeChild(this.wrapper.firstChild)
+    this.create()
   }
 }
